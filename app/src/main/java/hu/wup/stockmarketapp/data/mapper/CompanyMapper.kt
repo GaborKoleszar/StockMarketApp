68291972d84673890f008a5e26474547a9782b33
Gábor, Koleszár <gabor.koleszar@wup.hu>
2022-04-15 19:41:03 +0200

    Create daos and mapper

---
+++ app/src/main/java/hu/wup/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -0,0 +1,20 @@
+package hu.wup.stockmarketapp.data.mapper
+
+import hu.wup.stockmarketapp.data.local.CompanyListingEntity
+import hu.wup.stockmarketapp.domain.model.CompanyListing
+
+fun CompanyListingEntity.toCompanyListing(): CompanyListing {
+    return CompanyListing(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
+
+fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
+    return CompanyListingEntity(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
